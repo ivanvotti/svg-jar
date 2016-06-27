558fdba321cff8ebe498e98e436c4d8ebabb8cce
Ivan Votti <ivanvotti@gmail.com>
2016-06-27 15:15:55 +0700

    Setup SASS via node-sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,6 +2,11 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    sassOptions: {
+      // Fixes ember-cli-sass, that includes an outdated node-sass version,
+      // that doesn't work with Node.js 6
+      nodeSass: require('node-sass')
+    }
   });
 
   app.import(app.bowerDirectory + '/normalize-css/normalize.css');
