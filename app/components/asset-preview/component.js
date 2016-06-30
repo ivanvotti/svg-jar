292f8fd3b0d92c9272dd30d896082767e6e817c8
Ivan Votti <ivanvotti@gmail.com>
2016-06-30 20:33:30 +0700

    Improve asset scaling

--- app/components/asset-preview/component.js
+++ app/components/asset-preview/component.js
@@ -1,5 +1,14 @@
 import Component from 'ember-component';
+import computed from 'ember-computed';
+import get from 'ember-metal/get';
 
 export default Component.extend({
-  classNames: ['c-asset-preview']
+  classNames: ['c-asset-preview'],
+
+  isScaleUpNeeded: computed('asset', function() {
+    let height = get(this, 'asset.svg.height');
+    let width = get(this, 'asset.svg.width');
+
+    return Math.max(height, width) <= 50;
+  })
 });
