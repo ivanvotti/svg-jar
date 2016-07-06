544837466be98d241dbb9f76620cdbac10f9a435
Ivan Votti <ivanvotti@gmail.com>
2016-07-06 11:38:55 +0700

    Add ember-svg-jar

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,14 +2,20 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    fingerprint: {
+      enabled: false
+    },
+
     sassOptions: {
       // Fixes ember-cli-sass, that includes an outdated node-sass version,
       // that doesn't work with Node.js 6
       nodeSass: require('node-sass')
     },
 
-    fingerprint: {
-      enabled: false
+    svgJar: {
+      sourceDirs: ['svgs'],
+      strategy: 'symbol',
+      symbolsPrefix: 'icon-'
     }
   });
 
