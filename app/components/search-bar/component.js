f691c7a97e23c99588faca9ac7dadc654a545d4b
Ivan Votti <ivanvotti@gmail.com>
2016-07-19 19:53:15 +0700

    Improve search bar shortcuts

--- app/components/search-bar/component.js
+++ app/components/search-bar/component.js
@@ -1,10 +1,28 @@
 import Component from 'ember-component';
+import get from 'ember-metal/get';
 
 export default Component.extend({
   classNames: ['c-search-bar'],
   searchQuery: null,
 
+  blurInput() {
+    this.$('.js-search-bar-input').blur();
+  },
+
   actions: {
+    onEnter() {
+      this.blurInput();
+    },
+
+    onEscape() {
+      // Reset search query or blur if it's already empty.
+      if (!get(this, 'searchQuery')) {
+        this.blurInput();
+      }
+
+      this.attrs.setSearchQuery('');
+    },
+
     resetQuery() {
       this.attrs.setSearchQuery('');
     }
